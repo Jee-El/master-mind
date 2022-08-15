a3cc30938b1a380bac7c000fcdc90c55597843b3
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-08-15 16:02:41 +0100

    Use loop..do to ask for input for a cleaner code

--- platform_name.rb
+++ platform_name.rb
@@ -4,13 +4,14 @@ require 'tty-box'
 
 def platform_name
   platform_names_by_numbers = { '1' => 'Phone', '2' => 'Computer' }
+  puts
   puts TTY::Box.frame("1 -> Phone\n\n2 -> Computer",
                       padding: [1, 1],
-                      title: { top_center: 'Are you on a phone or a computer?' })
-  name = gets.chomp
-  until name.strip.match?(/^(1|2)$/)
-    print 'Enter either the number 1 or 2 : '
+                      title: { top_center: ' Are you on a phone or a computer? ' })
+  loop do
     name = gets.chomp
+    break platform_names_by_numbers[name] if name.strip.match?(/^(1|2)$/)
+
+    print 'Enter either the number 1 or 2 : '
   end
-  platform_names_by_numbers[name]
 end
