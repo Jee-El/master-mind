b59780ecb90ce31b573972f2053b68f5ed08b007
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-08-25 17:09:07 +0100

    Fix: class methods not set as private

--- hints.rb
+++ hints.rb
@@ -13,9 +13,7 @@ module MasterMind
       pegs.values
     end
 
-    private
-
-    def self.black_pegs(pegs, guess_secret_code_pairs, not_black_pegs_guess, not_black_pegs_secret_code)
+    private_class_method def self.black_pegs(pegs, guess_secret_code_pairs, not_black_pegs_guess, not_black_pegs_secret_code)
       guess_secret_code_pairs.each do |pair|
         next pegs[:black] += 1 if pair[0] == pair[1]
 
@@ -24,7 +22,7 @@ module MasterMind
       end
     end
 
-    def self.white_pegs(pegs, not_black_pegs_guess, not_black_pegs_secret_code)
+    private_class_method def self.white_pegs(pegs, not_black_pegs_guess, not_black_pegs_secret_code)
       pegs[:white] += (not_black_pegs_guess + not_black_pegs_secret_code).uniq.reduce(0) do |total, num|
         total += [not_black_pegs_guess.count(num), not_black_pegs_secret_code.count(num)].min
       end
