04379a071eaa4519aff5acd46284a343633a3306
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-08-25 17:44:46 +0100

    Use an even better way to figure out hints

--- hints.rb
+++ hints.rb
@@ -4,28 +4,20 @@ module MasterMind
   # Give hints to the code breaker
   class Hints
     def self.give(guess, secret_code)
-      not_black_pegs_guess = []
-      not_black_pegs_secret_code = []
       pegs = { black: 0, white: 0 }
-      guess_secret_code_pairs = guess.chars.zip(secret_code.chars)
-      black_pegs(pegs, guess_secret_code_pairs, not_black_pegs_guess, not_black_pegs_secret_code)
-      white_pegs(pegs, not_black_pegs_guess, not_black_pegs_secret_code)
+      black_pegs(pegs, guess, secret_code)
+      white_pegs(pegs, guess, secret_code)
       pegs.values
     end
 
-    private_class_method def self.black_pegs(pegs, guess_secret_code_pairs, not_black_pegs_guess, not_black_pegs_secret_code)
-      guess_secret_code_pairs.each do |pair|
-        next pegs[:black] += 1 if pair[0] == pair[1]
-
-        not_black_pegs_guess << pair[0]
-        not_black_pegs_secret_code << pair[1]
-      end
+    private_class_method def self.black_pegs(pegs, guess, secret_code)
+      4.times { |i| pegs[:black] += 1 if guess[i] == secret_code[i] }
     end
 
-    private_class_method def self.white_pegs(pegs, not_black_pegs_guess, not_black_pegs_secret_code)
-      pegs[:white] += (not_black_pegs_guess + not_black_pegs_secret_code).uniq.reduce(0) do |total, num|
-        total + [not_black_pegs_guess.count(num), not_black_pegs_secret_code.count(num)].min
-      end
+    private_class_method def self.white_pegs(pegs, guess, secret_code)
+      used_nums = (guess.chars + secret_code.chars).uniq
+      used_nums.each { |used_num| pegs[:white] += [guess.count(used_num), secret_code.count(used_num)].min }
+      pegs[:white] -= pegs[:black]
     end
   end
 end
