d28a66a0ebda0809f4fb8660b342cb4528de0417
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-08-27 23:36:05 +0100

    Put the platform_name method in a module

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require_relative './platform_name'
 require_relative './phone_game_setup'
 require_relative './computer_game_setup'
 
-case platform_name
+case MasterMind::PlatformName.get
 when 'Phone' then MasterMind::PhoneGameSetup.new.build_game
 when 'Computer' then MasterMind::ComputerGameSetup.new.build_game
 end
--- platform_name.rb
+++ platform_name.rb
@@ -2,16 +2,20 @@
 
 require 'tty-box'
 
-def platform_name
-  platform_names_by_numbers = { '1' => 'Phone', '2' => 'Computer' }
-  puts
-  puts TTY::Box.frame("1 -> Phone\n\n2 -> Computer",
-                      padding: [1, 1],
-                      title: { top_center: ' What device are you using? ' })
-  loop do
-    name = gets.chomp
-    break platform_names_by_numbers[name] if name.strip.match?(/^(1|2)$/)
+module MasterMind
+  module PlatformName
+    def self.get
+      platform_names_by_numbers = { '1' => 'Phone', '2' => 'Computer' }
+      puts
+      puts TTY::Box.frame("1 -> Phone\n\n2 -> Computer",
+                          padding: [1, 1],
+                          title: { top_center: ' What device are you using? ' })
+      loop do
+        name = gets.chomp
+        break platform_names_by_numbers[name] if name.strip.match?(/^(1|2)$/)
 
-    print 'Enter either the number 1 or 2 : '
+        print 'Enter either the number 1 or 2 : '
+      end
+    end
   end
 end
